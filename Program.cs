2c5299552320cbc7a88324afc8e430977fd858c2
mathe <mathe@DESKTOP-06LM2TJ>
2024-08-29 16:14:36 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula4_cs_ex3
+{
+
+    /*Escreva um programa em C#, seguindo as orientações do início deste documento. O
+    seu programa deve receber 3 valores a, b e c, lados de um triângulo, e verificar
+    qual é o tipo de triângulo formado retornando:
+    • 0 - se o triângulo é retângulo (A^2=B^2+C^2);
+    • 1 - se o triângulo é acutângulo (A^2 > B^2 + C^2);
+    • 2 - obtusângulo (A^2 < B^2 + C^2).
+    Considere que, para aplicar as relações mostradas, o programa deve garantir que
+    o maior dos 3 lados estará em A.
+    Testar o valor recebido e escrever o tipo do triângulo*/
+
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+        }
+        public Program()
+        {
+            Convert Converter = new Convert();
+            List<string> listaS = new List<string>();
+            List<double> listaD = new List<double>();
+            int retorno;
+
+            Console.WriteLine("Digite os 3 valores: ");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Digite o valor {0}/3: ", i + 1);
+                listaS.Add(Console.ReadLine());
+            }
+            listaD = listaS.ConvertAll(double.Parse);
+            Converter.Logica1(listaD);
+            retorno = Converter.Logica2(listaD);
+            if (retorno == 0)
+            {
+                Console.WriteLine("0 - Triângulo Retângulo");
+            }
+            else if (retorno == 1)
+            {
+                Console.WriteLine("1 - Triângulo Acutângulo");
+            }
+            else if (retorno == 2) 
+            {
+                Console.WriteLine("2 - Triângulo Obtusângulo");
+            }
+            else
+            { 
+                Console.WriteLine("Erro");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("aula4 cs ex3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("aula4 cs ex3")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("5e7748fc-b1ec-4d6e-8a92-1dfc2b468fe5")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
